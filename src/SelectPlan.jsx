46f1e054bfb35a83e0e3aa804584a4b001298328
stephenlit <stevish45@icloud.com>
2024-02-27 06:56:57 -0400

    added functionallty to SelectPlan componet

--- src/SelectPlan.jsx
+++ src/SelectPlan.jsx
@@ -1,29 +1,48 @@
 import { FormWrapper } from "./FormWrapper";
 
 export function SelectPlan({ plan, updateFields }) {
+  const handlePlanChange = (e) => {
+    updateFields({ plan: e.target.value });
+  };
+
   return (
     <FormWrapper title="Select Plan">
-      <label>Plan 1</label>
-      <input
-        type="radio"
-        name="plan"
-        value="1"
-        onClick={(e) => updateFields({ plan: e.target.value })}
-      />
-      <label>Plan 2</label>
-      <input
-        type="radio"
-        name="plan"
-        value="2"
-        onClick={(e) => updateFields({ plan: e.target.value })}
-      />
-      <label>Plan 3</label>
-      <input
-        type="radio"
-        name="plan"
-        value="3"
-        onClick={(e) => updateFields({ plan: e.target.value })}
-      />
+      <fieldset>
+        <legend>Select a Plan:</legend>
+        <div>
+          <input
+            type="radio"
+            id="plan1"
+            name="plan"
+            value="1"
+            checked={plan === "1"}
+            onChange={handlePlanChange}
+          />
+          <label htmlFor="plan1">Plan 1</label>
+        </div>
+        <div>
+          <input
+            type="radio"
+            id="plan2"
+            name="plan"
+            value="2"
+            checked={plan === "2"}
+            onChange={handlePlanChange}
+          />
+          <label htmlFor="plan2">Plan 2</label>
+        </div>
+        <div>
+          <input
+            type="radio"
+            id="plan3"
+            name="plan"
+            value="3"
+            checked={plan === "3"}
+            onChange={handlePlanChange}
+          />
+          <label htmlFor="plan3">Plan 3</label>
+        </div>
+      </fieldset>
     </FormWrapper>
   );
 }
